2282813bddee9ca21a6d2fecb17b9fceba55565c
southflorida15 <southflorida15@gmail.com>
2026-06-05 02:07:47 -0400

    Update live.js

--- API/live.js
+++ API/live.js
@@ -1,21 +1,5 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch(
-      'https://free-api-live-football-data.p.rapidapi.com/football-players-search?search=m',
-      {
-        headers: {
-          'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
-          'X-RapidAPI-Host': process.env.RAPIDAPI_HOST,
-        },
-      }
-    );
-
-    const data = await response.json();
-
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
-  }
+export default function handler(req, res) {
+  res.status(200).json({
+    message: "live endpoint works"
+  });
 }
